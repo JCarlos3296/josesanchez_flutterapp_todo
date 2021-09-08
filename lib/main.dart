54fb5e4144da728eba5a461e591c9acfb480c2e8
Jose Sanchez <josecarlos05032000@gmail.com>
2021-09-08 00:23:04 -0600

    feat: Started GetX.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:josesanchez_flutterapp_todo/src/modules/todo/ToDoPage.dart';
+import 'package:josesanchez_flutterapp_todo/src/counter_getx/CounterPage.dart';
 
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TodoPage(),
+      home: CounterPage(),
     );
   }
 }
